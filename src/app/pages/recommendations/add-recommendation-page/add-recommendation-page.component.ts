e68a9b4a0c24aa9ec8a161fb87ca9cf4f823936c
Mihail Bondarenco <bondarenco.mihail@gmail.com>
2021-01-07 09:58:23 +0100

    fix red heart asset path

--- src/app/pages/recommendations/add-recommendation-page/add-recommendation-page.component.ts
+++ src/app/pages/recommendations/add-recommendation-page/add-recommendation-page.component.ts
@@ -1,18 +1,19 @@
-import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { WikiEntitySelectorComponent } from 'src/app/components/wiki-entity-selector/wiki-entity-selector.component';
-import { Interest } from 'src/app/models/interest/Interest';
-import { Persona } from 'src/app/models/persona/Persona';
-import { AcceptedInterestsTypes, WikiInterestVM } from 'src/app/models/interest/WikiInterestVM';
-import { WikiPersonVM } from 'src/app/models/persona/WikiPersonVM';
-import { CreateRecommendationVM } from 'src/app/models/recommendations/CreateRecommendationVM';
-import { Recommendation } from 'src/app/models/recommendations/Recommendation';
-import { WikiIdentifier } from 'src/app/models/wiki/WikiIdentifier';
-import { InterestsService } from 'src/app/services/interests-service.service';
-import { PersonasService } from 'src/app/services/personas.service';
-import { RecommendationsService } from 'src/app/services/recommendations.service';
+import { HttpResponse } from '@angular/common/http'
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { WikiEntitySelectorComponent } from 'src/app/components/wiki-entity-selector/wiki-entity-selector.component'
+import { Interest } from 'src/app/models/interest/Interest'
+import { Persona } from 'src/app/models/persona/Persona'
+import { AcceptedInterestsTypes, WikiInterestVM } from 'src/app/models/interest/WikiInterestVM'
+import { WikiPersonVM } from 'src/app/models/persona/WikiPersonVM'
+import { CreateRecommendationVM } from 'src/app/models/recommendations/CreateRecommendationVM'
+import { Recommendation } from 'src/app/models/recommendations/Recommendation'
+import { WikiIdentifier } from 'src/app/models/wiki/WikiIdentifier'
+import { InterestsService } from 'src/app/services/interests-service.service'
+import { PersonasService } from 'src/app/services/personas.service'
+import { RecommendationsService } from 'src/app/services/recommendations.service'
+import { Images } from 'src/app/utils/Images'
 
 @Component({
   selector: 'app-add-recommendation-page',
@@ -29,21 +30,22 @@ export class AddRecommendationPageComponent implements OnInit {
   persona: Persona = null
   interest: Interest = null
 
-  constructor(
+  HEART_IMAGE = Images.RedHeart
+
+  constructor (
     private service: RecommendationsService,
     private personasService: PersonasService,
     private interestsService: InterestsService,
     private router: Router,
-  )
-  {
+  ) {
     const nav = this.router.getCurrentNavigation()
-    if (nav && nav.extras.state ) {
+    if (nav && nav.extras.state) {
       this.persona = nav.extras.state.persona
       this.model.personaId = this.persona.id
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   personaSelected(model: WikiPersonVM): void {
 
@@ -64,22 +66,22 @@ export class AddRecommendationPageComponent implements OnInit {
       type: AcceptedInterestsTypes[model.instanceOf]
     })
 
-    this.interestsService.create(model).subscribe( interest => {
+    this.interestsService.create(model).subscribe(interest => {
       this.interest = Interest.AttachMethods(interest)
       this.model.interestId = interest.id
     })
 
   }
 
-  addRecommendation(): void{
+  addRecommendation(): void {
     const currentPage = window.location.href
 
     this.service.createAndObserveResponse(this.model).subscribe({
       next: (response: HttpResponse<Recommendation>) => {
-        if (currentPage === window.location.href){
+        if (currentPage === window.location.href) {
           this.router.navigate(['recommendations', response.body.id])
         }
-        switch (response.status){
+        switch (response.status) {
           case 200:
             console.log('recommendation already existed')
             break
@@ -90,9 +92,9 @@ export class AddRecommendationPageComponent implements OnInit {
     })
   }
 
-  get PersonaModelMapper(): any { return WikiPersonVM.PersonaModelMapper}
-  get PersonaEntityFilter(): any { return WikiPersonVM.PersonaEntityFilter}
-  get InterestModelMapper(): any { return WikiInterestVM.InterestModelMapper}
-  get InterestEntityFilter(): any { return WikiInterestVM.InterestEntityFilter}
+  get PersonaModelMapper(): any { return WikiPersonVM.PersonaModelMapper }
+  get PersonaEntityFilter(): any { return WikiPersonVM.PersonaEntityFilter }
+  get InterestModelMapper(): any { return WikiInterestVM.InterestModelMapper }
+  get InterestEntityFilter(): any { return WikiInterestVM.InterestEntityFilter }
 
 }
